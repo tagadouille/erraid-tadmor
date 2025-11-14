5c2173e4447acdf97cb1615d89500e2bd3709684
MarcY <marcyounan2005@gmail.com>
2025-11-14 10:06:09 +0100

    deleted unused files

--- src/main.c
+++ src/main.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#include "operation.h"
-#include "test/test.h"
 #include "tree-reading/tree_reader.h"
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Sum of 3 and 5 is: %d\n", sum(3, 5));
-    printf("Multiply of 4 and 6 is: %d\n", multiply(4, 6));
-
     printf("Running tests...\n\n");
 
     printf("Test for listing\n");
