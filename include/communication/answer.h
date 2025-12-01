150accd05aed8f7504b5da2d6380f4ce30851bc6
daie <elias.dai@etu.u-paris.fr>
2025-12-01 18:12:56 +0100

    Création des types de données pour les requêtes

---
+++ include/communication/answer.h
@@ -0,0 +1,101 @@
+#include "communication/code.h"
+#include <stdint.h>
+#include "types/time_exitcode.h"
+#include "types/task.h"
+
+/**
+ * For the basic answer and for the
+ * errors
+ */
+typedef struct{
+
+    uint16_t anstype;
+    uint64_t task_id;
+    uint16_t errcode;
+
+}answer_t;
+
+/**
+ * For the answer to STDOUT and STDERR
+ */
+typedef struct{
+
+    uint16_t anstype;
+    string_t output;
+    uint16_t errcode;
+
+}a_output_t;
+
+/**
+ * For the answer to time_exitcode
+ */
+typedef struct{
+
+    uint16_t anstype;
+    uint32_t nbrun;
+    time_exitcode_t* all_timecode; //An array of size nbrun
+
+}a_timecode_t;
+
+/**
+ * For the answer to list
+ */
+typedef struct{
+
+    uint16_t anstype;
+    uint32_t nbtask;
+    task_t* all_task; //Array of size of nbtask
+
+}a_list_t;
+
+/**
+ * @brief fill and create the answer structure
+ * @param anstype the type of the answer
+ * @param task_id the id of the task
+ * @param errcode the error code if there's an error
+ */
+answer_t* create_answer(uint16_t anstype, uint64_t task_id, uint16_t errcode);
+
+/**
+ * @brief fill and create the answer list structure
+ * @param anstype the type of the answer
+ * @param nbtask the number of task
+ * @param all_task an array of task of size nbtask
+ */
+a_list_t* create_a_list(uint16_t anstype, uint32_t nbtask, task_t* all_task);
+
+/**
+ * @brief fill and create the answer timecode structure
+ * @param anstype the type of the answer
+ * @param nbrun the number of time exitcode
+ * @param all_timecode an array of time_exitcode_t of size nbrun
+ */
+a_timecode_t* create_a_timecode_t(uint16_t anstype, uint32_t nbrun, time_exitcode_t* all_timecode);
+
+/**
+ * @brief fill and create the answer output structure
+ * @param anstype the type of the answer
+ * @param output the output of a task
+ * @param errcode the error code if there's an error
+ */
+a_output_t* create_a_output_t(uint16_t anstype, string_t output, uint16_t errcode);
+
+/**
+ * @brief free the answer structure
+ */
+void free_answer(answer_t* answer);
+
+/**
+ * @brief free the answer list structure
+ */
+void free_a_list(a_list_t* list);
+
+/**
+ * @brief free the answer timecode structure
+ */
+void free_a_timecode_t(a_timecode_t* timecode);
+
+/**
+ * @brief free the answer output structure
+ */
+void free_a_output_t(a_output_t* output);
---
+++ include/communication/code.h
@@ -0,0 +1,37 @@
+#define PIPE_BUF 4096
+
+/**
+ * The enum of the OPCODE
+ */
+enum opcode{
+    LS = 0x4c53, //LIST -- list all the task
+    CR = 0x4352, //CREATE -- create a new simple task
+    CB = 0x4342, //COMBINE -- create a new task per combination of already existing task
+    RM = 0x524d, // REMOVE -- delete a task
+    TX = 0x5458, // TIMES_EXITCODES -- display times_exitcodes file
+    SO = 0x534f, // STDOUT -- display stdout of the last complete execution of a task
+    SE = 0x5345, // STDERR -- display stderr of the last complete execution of a task
+    TM = 0x4b49 //TERMINATE -- finish the daemon
+};
+
+typedef enum opcode opcode_t;
+
+/**
+ * The enum of the ANSTYPE
+ */
+enum anstype{
+    OK = 0x4f4b,
+    ERR = 0x4552,
+};
+
+typedef enum anstype anstype_t;
+
+/**
+ * The enum of the ERRCODE
+ */
+enum errcode{
+    NF = 0x4e46,
+    NR = 0x4e52
+};
+
+typedef enum errcode errcode_t;
