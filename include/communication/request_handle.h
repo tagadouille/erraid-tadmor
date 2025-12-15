2d47cdca7c5b8100faf808b2fd4f12e2888415b8
linc <catherine.lin@etu.u-paris.fr>
2025-12-15 20:28:07 +0100

    Add comments for request_handle

--- include/communication/request_handle.h
+++ include/communication/request_handle.h
@@ -9,14 +9,37 @@
 #include "communication/answer.h"
 #include "communication/request.h"
 
+/**
+ * @brief Create a list of all tasks.
+ * @param rundir Base directory of task folders
+ */
 a_list_t *handle_ls(char *rundir);
 
-a_timecode_t *handle_tx(char *rundir, uint64_t id);
-
-a_output_t *handle_output(char *rundir, uint64_t id, bool is_stder);
-
+/**
+ * @brief Remove a task folder.
+ * @param rundir Base directory of task folders
+ * @param id Task id
+ */
 answer_t *handle_rm(char *rundir, uint64_t id);
 
+/**
+ * @brief Retrieve time-exitcodes of a task.
+ * @param rundir Base directory of task folders
+ * @param id Task id
+ */
+a_timecode_t *handle_tx(char *rundir, uint64_t id);
+
+/**
+ * @brief Retrieve output or error output of a task.
+ * @param rundir Base directory of task folders
+ * @param id Task id
+ * @param is_stderr true = stderr, false = stdout
+ */
+a_output_t *handle_output(char *rundir, uint64_t id, bool is_stder);
+
+/**
+ * @brief Terminate the daemon.
+ */
 answer_t *handle_tm(void);
 
 /**
