4bd07deee39ac0d289f31fed873560a6df516053
Catherine Lin <catherine.lin@etu.u-paris.fr>
2025-12-23 04:10:30 +0100

    test 13

--- src/erraids/erraid-scanner.c
+++ src/erraids/erraid-scanner.c
@@ -10,7 +10,7 @@
 /**
  * All the tasks that was scanned
  */
-static all_task_t* scanned_tasks = NULL;
+static all_task_t *scanned_tasks = NULL;
 
 /* ------------------------------ Timing util ---------------------------- */
 /* wait until the next minute boundary (sleep until seconds == 0) */
@@ -22,7 +22,8 @@ static time_t wait_next_minute(void)
     ts.tv_sec = ts.tv_sec - (ts.tv_sec % 60) + 60;
     ts.tv_nsec = 0;
 
-    while (clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &ts, NULL) == EINTR);
+    while (clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &ts, NULL) == EINTR)
+        ;
 
     return ts.tv_sec;
 }
@@ -30,13 +31,15 @@ static time_t wait_next_minute(void)
 /**
  * @brief Scan all the task available in the tasksdir
  */
-static void scan_all_task(){
+static void scan_all_task()
+{
 
     write_log_msg("Scanning tasks directory…");
 
     scanned_tasks = all_task_listing(tasksdir);
 
-    if(scanned_tasks == NULL){
+    if (scanned_tasks == NULL)
+    {
         write_log_msg("Error : an error occured while scanning all the tasks of path %s", tasksdir);
         running = 0;
         return;
@@ -46,32 +49,43 @@ static void scan_all_task(){
 
 /**
  * @brief Execute all the task that was scanned
- * @param minute_now the actual minute 
+ * @param minute_now the actual minute
  */
-static void execute_all_task(time_t minute_now){
+static void execute_all_task(time_t minute_now)
+{
 
-    if(scanned_tasks == NULL){
+    if (scanned_tasks == NULL)
+    {
         write_log_msg("Error : scanned_tasks is NULL, can't execute all the tasks");
         return;
     }
 
-    for (uint32_t i = 0; i < scanned_tasks -> nbtask; i++)
+    for (uint32_t i = 0; i < scanned_tasks->nbtask; i++)
     {
-        //task_display(&(scanned_tasks -> all_task)[i]);
-        run_task_if_due(&(scanned_tasks -> all_task)[i], minute_now);
+        // task_display(&(scanned_tasks -> all_task)[i]);
+        // run_task_if_due(&(scanned_tasks -> all_task)[i], minute_now);
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            run_task_if_due(&(scanned_tasks->all_task)[i], minute_now);
+            _exit(0);
+        }
     }
+    while (waitpid(-1, NULL, WNOHANG) > 0);
 
     write_log_msg("The execution is finish ! Go back to sleep.. zzz..\n");
 }
 
-void erraid_scan_loop(){
+void erraid_scan_loop()
+{
 
     write_log_msg("Daemon main loop started.");
     // Scan of the tasks of the directory
     scan_all_task();
 
     // Execution loop of the tasks
-    while (running) {
+    while (running)
+    {
 
         time_t minute_now = wait_next_minute();
 
