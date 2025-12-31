b7679ede8da5058e240fd4483af5b33f6a34152c
linc <catherine.lin@etu.u-paris.fr>
2025-12-31 11:21:33 +0100

    Writing functions

--- include/tree-writer/task_creator.h
+++ include/tree-writer/task_creator.h
@@ -15,8 +15,18 @@ uint64_t find_next_id(const char *base_path);
  * @brief Write the timing file.
  * @param fd file descriptor.
  * @param t timing to put in the file.
+ * @return 0 on success and -1 on failure.
  */
 int write_timing_file(int fd, const timing_t *t);
+
+/**
+ * @brief Write a simple task command (SI) to a file descriptor.
+ * @param fd file descriptor.
+ * @param args arguments of the command.
+ * @return 0 on success, -1 on failure.
+ */
+int write_command_simple(int fd, const arguments_t *args);
+
 
 
 #endif
