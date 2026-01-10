c267985b1b96a1cae0e8a9f19451fb9ae8ebdc48
MarcY <marcyounan2005@gmail.com>
2026-01-10 11:52:54 +0100

    erraid start main

--- src/erraids/erraid.c
+++ src/erraids/erraid.c
@@ -20,6 +20,8 @@ task_t* curr_task = NULL;
 
 volatile int running = 1;
 
+static pid_t servant_pid = -1;
+
 
 /**
  * The current log path
@@ -38,17 +40,66 @@ time_array_t* curr_time = NULL;
 
 int g_foreground_mode = 0;
 
+/**
+ * @brief Start the servant process
+ */
+static void start_son(){
+
+    servant_pid = fork();
+
+    if (servant_pid < 0) {
+        perror("fork");
+        return;
+    }
+
+    if (servant_pid == 0) {
+        // servant
+        start_serve(getppid());
+        _exit(EXIT_SUCCESS);
+    }
+}
+
 /* --------------------------- SIGNAL HANDLER ---------------------------- */
+
+static void handle_sigchld(int sig) {
+    (void)sig;
+    int saved_errno = errno;
+    pid_t pid;
+
+    // Reap all terminated children to avoid zombies
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+        if (pid == servant_pid) {
+            servant_pid = -1; // Mark servant as dead
+        }
+    }
+    errno = saved_errno;
+}
+
+static void handle_sigusr2(int sig){
+    (void)sig;
+    int saved_errno = errno;
+    pid_t pid;
+
+    // Reap all terminated children to avoid zombies
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+        if (pid == servant_pid) {
+            servant_pid = -1; // Mark servant as dead
+        }
+    }
+
+    dprintf(STDOUT_FILENO, "Handled SIGUSR2, reaped child processes.\n");
+
+    start_son();
+
+    errno = saved_errno;
+}
 
 static void handle_signal(int sig) {
     (void)sig;
     running = 0;
 }
 
-static void handle_sigchld(int sig) {
-    (void)sig;
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-}
+
 /* ---------------------------- DAEMON MODE ------------------------------ */
 
 int daemon_init(void) {
@@ -60,6 +111,11 @@ int daemon_init(void) {
     }
 
     if (ensure_rundir() != 0) return -1;
+
+    if(write_pid_file() < 0){
+        dprintf(STDERR_FILENO, "Error : can't write the pid file\n");
+        return -1;
+    }
 
     if (mkdir_p(pipe_path) != 0) {
         perror("mkdir_p(pipe_path)");
@@ -108,8 +164,11 @@ int daemon_init(void) {
 
     struct sigaction sa_chld = {0};
     sa_chld.sa_handler = handle_sigchld;
-    sa_chld.sa_flags = SA_RESTART;
     sigaction(SIGCHLD, &sa_chld, NULL);
+
+    struct sigaction sa_usr = {0};
+    sa_usr.sa_handler = handle_sigusr2;
+    sigaction(SIGUSR2, &sa_usr, NULL);
 
     struct sigaction sa = {0};
     sa.sa_handler = handle_signal;
@@ -125,24 +184,10 @@ int daemon_init(void) {
 
 void daemon_run(void) {
 
-    pid_t servant_pid;
-
-    servant_pid = fork();
-    if (servant_pid < 0) {
-        perror("fork");
-        return;
-    }
-
-    if (servant_pid == 0) {
-        // servant
-        start_serve(getppid());
-        _exit(EXIT_SUCCESS);
-    }
+    start_son();
 
     // scanner (father)
     erraid_scan_loop();
 
-    // Stop servant
-    kill(servant_pid, SIGTERM);
-    waitpid(servant_pid, NULL, 0);
+    daemon_cleanup();
 }
